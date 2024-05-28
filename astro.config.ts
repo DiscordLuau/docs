83b9b1f47315d52be7bed22317f697b1f37ba29c
Erica Marigold <hi@devcomp.xyz>
2024-05-28 18:38:56 +0530

    docs: include slash commands guide

--- astro.config.ts
+++ astro.config.ts
@@ -19,13 +19,9 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: "Guides",
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{
-							label: "Example Guide",
-							link: "/guides/example/",
-						},
-					],
+					autogenerate: {
+						directory: "guides",
+					},
 				},
 				{
 					label: "Classes",
