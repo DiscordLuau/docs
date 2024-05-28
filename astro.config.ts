db70eb129e6d0bbc986123f7ddd8e118fbde154f
Erica Marigold <hi@devcomp.xyz>
2024-05-28 17:41:31 +0530

    refactor: cleanup code + prettier setup

--- astro.config.ts
+++ astro.config.ts
@@ -7,31 +7,41 @@ export default defineConfig({
   output: "server",
   adapter: deno(),
   image: {
-    service: passthroughImageService()
+    service: passthroughImageService(),
   },
-  integrations: [starlight({
-    title: "DiscordLuau Docs",
-    social: {
-      github: "https://github.com/DiscordLuau/discord-luau"
-    },
-    sidebar: [{
-      label: "Guides",
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: "Example Guide",
-        link: "/guides/example/"
-      }]
-    }, {
-      label: "Classes",
-      autogenerate: {
-        directory: "classes"
-      }
-    }],
-    customCss: ['./src/styles/landing.css', './src/styles/starlight.css', './src/styles/lua.css'],
-    components: {
-      Hero: "./src/components/Hero.astro",
-      Pagination: './src/components/Pagination.astro',
-    }
-  })]
+  integrations: [
+    starlight({
+      title: "DiscordLuau Docs",
+      social: {
+        github: "https://github.com/DiscordLuau/discord-luau",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Example Guide",
+              link: "/guides/example/",
+            },
+          ],
+        },
+        {
+          label: "Classes",
+          autogenerate: {
+            directory: "classes",
+          },
+        },
+      ],
+      customCss: [
+        "./src/styles/landing.css",
+        "./src/styles/starlight.css",
+        "./src/styles/lua.css",
+      ],
+      components: {
+        Hero: "./src/components/Hero.astro",
+        Pagination: "./src/components/Pagination.astro",
+      },
+    }),
+  ],
 });
--- src/components/utils/lua.ts
+++ src/components/utils/lua.ts
@@ -1,69 +1,76 @@
 export type LuaType =
-    | "nil"
-    | ("boolean" | "bool")
-    | "number"
-    | "string"
-    | "table"
-    | ("tuple" | "...")
-    | ("userdata" | "proxy");
+  | "nil"
+  | ("boolean" | "bool")
+  | "number"
+  | "string"
+  | "table"
+  | ("tuple" | "...")
+  | ("userdata" | "proxy");
 
 export type LuaParam = {
-    name: string;
-    type: LuaType | string;
+  name: string;
+  type: LuaType | string;
 };
 
 export const luaTypeDocs: Record<LuaType, string> = {
-    nil: "2.1",
-    boolean: "2.2",
-    bool: "2.2",
-    number: "2.3",
-    string: "2.4",
-    table: "2.5",
-    tuple: "5.1",
-    "...": "5.1",
-    userdata: "28.1",
-    proxy: "28.1",
+  nil: "2.1",
+  boolean: "2.2",
+  bool: "2.2",
+  number: "2.3",
+  string: "2.4",
+  table: "2.5",
+  tuple: "5.1",
+  "...": "5.1",
+  userdata: "28.1",
+  proxy: "28.1",
 };
 
-export const getLuaDocs = (luaType: LuaType) => {
-    const luaDocsBaseUrl = new URL("https://www.lua.org/pil");
-    luaDocsBaseUrl.pathname += "/" + luaTypeDocs[luaType] + ".html";
+export const removeTypeModifiers = (type: string) => {
+  return type.replaceAll(/\?/g, "") as LuaType;
+};
 
-    return luaDocsBaseUrl.toString();
+export const getLuaDocs = (luaType: LuaType | string) => {
+  const normalizedType = removeTypeModifiers(luaType);
+  const luaDocsBaseUrl = new URL("https://www.lua.org/pil");
+  luaDocsBaseUrl.pathname += "/" + luaTypeDocs[normalizedType] + ".html";
+
+  return luaDocsBaseUrl.toString();
 };
 
 export const getCustomTypeDocs = (type: string) => {
-    if (type == "()" || type == "any") {
-        return null;
-    }
+  if (type == "()" || type == "any") {
+    return null;
+  }
 
-    const path =
-        "/classes/" +
-        type
-            .split(".")
-            .map((component) => component.toLocaleLowerCase())
-            .join("/");
+  const path =
+    "/classes/" +
+    type
+      .split(".")
+      .map((component) => component.toLocaleLowerCase())
+      .join("/");
 
-    return path;
+  return path;
 };
 
 export const getTypeDocs = (type: string) => {
-    return type in luaTypeDocs
-        ? getLuaDocs(type as LuaType)
-        : getCustomTypeDocs(type);
+  return type in luaTypeDocs
+    ? getLuaDocs(type as LuaType)
+    : getCustomTypeDocs(type);
 };
 
 export const typeOrDefault = (type?: string) => {
-    return type ?? "()";
+  return type ?? "()";
 };
 
 export const getKvPairs = (tblInner: string) => {
-    tblInner = tblInner.trim()
-    const pairs = tblInner.split(",").map(pair => pair.split(":").map(elem => elem.trim()))
+  tblInner = tblInner.trim();
+  const pairs = tblInner
+    .split(",")
+    .map((pair) => pair.split(":").map((elem) => elem.trim()));
 
-    return pairs
-}
+  return pairs;
+};
 
 export const isObject = (tblInner: string) => {
-    return tblInner.match(/(.*):(.*)/) !== null;
-}
+  return tblInner.match(/(.*):(.*)/) !== null;
+};
