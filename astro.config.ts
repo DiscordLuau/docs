b522c3a378c04555f43bb0f3b9fcb9437504b0b7
Erica Marigold <hi@devcomp.xyz>
2024-05-28 17:55:14 +0530

    fix(lua.ts): handle types with modifiers correctly
    Also fixed some prettier configs, using tabs now.

--- astro.config.ts
+++ astro.config.ts
@@ -4,44 +4,44 @@ import deno from "@deno/astro-adapter";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: deno(),
-  image: {
-    service: passthroughImageService(),
-  },
-  integrations: [
-    starlight({
-      title: "DiscordLuau Docs",
-      social: {
-        github: "https://github.com/DiscordLuau/discord-luau",
-      },
-      sidebar: [
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            {
-              label: "Example Guide",
-              link: "/guides/example/",
-            },
-          ],
-        },
-        {
-          label: "Classes",
-          autogenerate: {
-            directory: "classes",
-          },
-        },
-      ],
-      customCss: [
-        "./src/styles/landing.css",
-        "./src/styles/starlight.css",
-        "./src/styles/lua.css",
-      ],
-      components: {
-        Hero: "./src/components/Hero.astro",
-        Pagination: "./src/components/Pagination.astro",
-      },
-    }),
-  ],
+	output: "server",
+	adapter: deno(),
+	image: {
+		service: passthroughImageService(),
+	},
+	integrations: [
+		starlight({
+			title: "DiscordLuau Docs",
+			social: {
+				github: "https://github.com/DiscordLuau/discord-luau",
+			},
+			sidebar: [
+				{
+					label: "Guides",
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{
+							label: "Example Guide",
+							link: "/guides/example/",
+						},
+					],
+				},
+				{
+					label: "Classes",
+					autogenerate: {
+						directory: "classes",
+					},
+				},
+			],
+			customCss: [
+				"./src/styles/landing.css",
+				"./src/styles/starlight.css",
+				"./src/styles/lua.css",
+			],
+			components: {
+				Hero: "./src/components/Hero.astro",
+				Pagination: "./src/components/Pagination.astro",
+			},
+		}),
+	],
 });
