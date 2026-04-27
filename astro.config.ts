5633b021b31ec6290ab57eeef5a2a4a393068756
AsynchronousMatrix <hi@asyncmatrix.dev>
2026-04-27 15:35:15 +0100

    fix: remove duplicate defineConfig import

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,3 @@
-import { defineConfig } from "astro/config";
 import { defineConfig, passthroughImageService } from "astro/config";
 import starlight from "@astrojs/starlight";
 import deno from "@deno/astro-adapter";
