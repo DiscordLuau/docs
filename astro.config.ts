c2a026d2d53a5ba28559dbdefec712d980f556d8
Erica Marigold <hi@devcomp.xyz>
2026-05-16 17:27:25 +0100

    feat: divorce deno, migrate to cloudflare pages

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import starlight from "@astrojs/starlight";
-import deno from "@deno/astro-adapter";
-
+import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
-	adapter: deno(),
+	adapter: cloudflare(),
 	image: {
 		service: passthroughImageService(),
 	},
@@ -18,121 +17,179 @@ export default defineConfig({
 			},
 			favicon: "/icon.png",
 			social: [
-				{ icon: "github", label: "GitHub", href: "https://github.com/DiscordLuau/discord-luau" },
-				{ icon: "discord", label: "Discord", href: "https://discord.gg/DpQwdD8zD3" },
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/DiscordLuau/discord-luau",
+				},
+				{
+					icon: "discord",
+					label: "Discord",
+					href: "https://discord.gg/DpQwdD8zD3",
+				},
 			],
 			sidebar: [
 				{
 					label: "Getting Started",
-					autogenerate: {
-						directory: "getting-started",
-					},
+					items: [{ autogenerate: { directory: "getting-started" } }],
 				},
 				{
 					label: "Guides",
-					autogenerate: {
-						directory: "guides",
-					},
+					items: [{ autogenerate: { directory: "guides" } }],
 				},
 				// Core - what most consumers interact with directly
 				{
 					label: "Discord Luau",
 					collapsed: false,
-					autogenerate: { directory: "classes/discordluau", collapsed: true },
+					items: [
+						{
+							autogenerate: {
+								directory: "classes/discordluau",
+								collapsed: true,
+							},
+						},
+					],
 				},
 				{
 					label: "Classes",
 					collapsed: false,
-					autogenerate: { directory: "classes/classes", collapsed: true },
+					items: [
+						{ autogenerate: { directory: "classes/classes", collapsed: true } },
+					],
 				},
 				{
 					label: "Builders",
 					collapsed: false,
-					autogenerate: { directory: "classes/builders", collapsed: true },
+					items: [
+						{
+							autogenerate: { directory: "classes/builders", collapsed: true },
+						},
+					],
 				},
 				{
 					label: "Types",
 					collapsed: false,
-					autogenerate: { directory: "classes/types", collapsed: true },
+					items: [
+						{ autogenerate: { directory: "classes/types", collapsed: true } },
+					],
 				},
 				{
 					label: "Rest",
 					collapsed: false,
-					autogenerate: { directory: "classes/rest", collapsed: true },
+					items: [
+						{ autogenerate: { directory: "classes/rest", collapsed: true } },
+					],
 				},
 				// Secondary - feature areas
 				{
 					label: "Voice",
 					collapsed: false,
-					autogenerate: { directory: "classes/voice", collapsed: true },
+					items: [
+						{ autogenerate: { directory: "classes/voice", collapsed: true } },
+					],
 				},
 				{
 					label: "State",
 					collapsed: false,
-					autogenerate: { directory: "classes/state", collapsed: true },
+					items: [
+						{ autogenerate: { directory: "classes/state", collapsed: true } },
+					],
 				},
 				{
 					label: "Emitter",
 					collapsed: false,
-					autogenerate: { directory: "classes/emitter", collapsed: true },
+					items: [
+						{ autogenerate: { directory: "classes/emitter", collapsed: true } },
+					],
 				},
 				{
 					label: "Utilities",
 					collapsed: false,
-					autogenerate: { directory: "classes/utilities", collapsed: true },
+					items: [
+						{
+							autogenerate: { directory: "classes/utilities", collapsed: true },
+						},
+					],
 				},
 				// Internal / infrastructure
 				{
 					label: "Snowflake",
 					collapsed: false,
-					autogenerate: { directory: "classes/snowflake", collapsed: true },
+					items: [
+						{
+							autogenerate: { directory: "classes/snowflake", collapsed: true },
+						},
+					],
 				},
 				{
 					label: "Logger",
 					collapsed: false,
-					autogenerate: { directory: "classes/logger", collapsed: true },
+					items: [
+						{ autogenerate: { directory: "classes/logger", collapsed: true } },
+					],
 				},
 				{
 					label: "Formdata",
 					collapsed: false,
-					autogenerate: { directory: "classes/formdata", collapsed: true },
+					items: [
+						{
+							autogenerate: { directory: "classes/formdata", collapsed: true },
+						},
+					],
 				},
 				{
 					label: "Stream",
 					collapsed: false,
-					autogenerate: { directory: "classes/stream", collapsed: true },
+					items: [
+						{ autogenerate: { directory: "classes/stream", collapsed: true } },
+					],
 				},
 				{
 					label: "Secret",
 					collapsed: false,
-					autogenerate: { directory: "classes/secret", collapsed: true },
+					items: [
+						{ autogenerate: { directory: "classes/secret", collapsed: true } },
+					],
 				},
 				{
 					label: "Buffer",
 					collapsed: false,
-					autogenerate: { directory: "classes/buffer", collapsed: true },
+					items: [
+						{ autogenerate: { directory: "classes/buffer", collapsed: true } },
+					],
 				},
 				{
 					label: "Bit",
 					collapsed: false,
-					autogenerate: { directory: "classes/bit", collapsed: true },
+					items: [
+						{ autogenerate: { directory: "classes/bit", collapsed: true } },
+					],
 				},
 				{
 					label: "Web Socket",
 					collapsed: false,
-					autogenerate: { directory: "classes/websocket", collapsed: true },
+					items: [
+						{
+							autogenerate: { directory: "classes/websocket", collapsed: true },
+						},
+					],
 				},
 				{
 					label: "STD Polyfills",
 					collapsed: false,
-					autogenerate: { directory: "classes/stdpolyfills", collapsed: true },
+					items: [
+						{
+							autogenerate: {
+								directory: "classes/stdpolyfills",
+								collapsed: true,
+							},
+						},
+					],
 				},
 				{
 					label: "Vendor",
 					collapsed: false,
-					autogenerate: {
-						directory: "vendor",
-					},
+					items: [{ autogenerate: { directory: "vendor" } }],
 				},
 			],
 			customCss: [
